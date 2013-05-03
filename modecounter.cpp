f355cbf84c305ed1f5c09b469a197248868f1e1c
sdyy1990 <sdyy1990@gmail.com>
2013-05-03 11:38:55 +0800

    before game

--- modecounter.cpp
+++ modecounter.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int map_n;
 int graph[32][32];
@@ -71,14 +73,28 @@ int insert(int &root) {
    
 
 }
-int  dfs(int ,int );
+int  dfs(int ,int ,int);
+void doSubGraphDegreeCount(int);
 typedef pair<int,int> P2;
 typedef map<P2,int> MP2;
 MP2 mapcnt;
 int main(int argc, char * argv[])
 {
+//   printf("argc = %d\n",argc);
+   if (argc == 1) {
+     printf("usage of modecounter:\n");
+     printf("-----------graph.txt must exist\n");
+     printf("modecounter [gameid] : count modes of gameid, gameid<0: gameid for all, input : stdin,\n\n");
+     printf("modecounter [gameid] C: count sub-graph-degree-modes of gameid \n"); 
+     printf("output format: Ggameid, mode ,count \n");
+     return 0;
+   }
    int gameid;
    sscanf(argv[1],"%d",&gameid);
+   //printf("%d",gameid);
+   if (argc == 3) {
+      doSubGraphDegreeCount(gameid); return 0;
+   }
    mapcnt.clear();
    int totcount = 0;
    while (cin >> s) {
@@ -96,21 +112,15 @@ int main(int argc, char * argv[])
       int thereismore = 1;
       while (thereismore) thereismore =insert(root);
       while (father[root] !=0) root = father[root];
-//      printf("root is %d\n",root);
-      /*for (int i = 0 ; i < treesize; i++) {
-         printf("%d:%14d%17d%4d%4d%4d\n",i,lch[i],brother[i],data[i],data_enable[i],father[i]);
-      }
-*/
       memset(degree,0,sizeof(degree));
       for (int i = 0 ; i < map_n; i++)
          for (int j = 0 ; j < map_n; j++)
             if (graph[i][j]) degree[i]++;
-      dfs(root,0);
+      dfs(root,0,0);
    }
    int ans[32][32];
    memset(ans,0,sizeof(ans));
       for (MP2::iterator iMP = mapcnt.begin(); iMP!=mapcnt.end(); iMP++) {
-//         cout << (iMP->first).first<<"\t"<<(iMP->first).second<<"\t"<<iMP->second << endl;
            ans[(iMP->first).first][(iMP->first).second] = iMP->second;
       }
    for (int i = 1; i<=20; i++) {
@@ -119,22 +129,60 @@ int main(int argc, char * argv[])
       printf("\n");
    }
 }
-
-int dfs(int root,int mask) {
+map<string,int> mapcnt2;
+void doSubGraphDegreeCount(int gameid) {
+    mapcnt2.clear();
+    while (cin >> s) { if (s[0]!='C') continue;
+       int mgameid; char s1[100]; char *p = const_cast<char *> (s.c_str()); 
+       sscanf(p,"C%[^,]",s1); sscanf(s1,"%d",&mgameid); 
+       if (mgameid!=gameid) continue;
+       readgraph(mgameid);
+  //     cout << s<< endl;
+       cleantree(); int root = 0, thereismore = 1;
+       while (thereismore) thereismore = insert(root);
+       while (father[root]!=0) root = father[root];
+      // for (int i = 0 ; i < treesize; i++) 
+       //  printf("%3d %3d %3d %3d\n", father[i], lch[i],brother[i],data[i]);
+       memset(degree,0,sizeof(degree));
+       for (int i = 0 ; i < map_n; i++)
+          for (int j = 0 ; j < map_n; j++)
+             if (graph[i][j]) degree[i]++;
+       dfs(root,0,1);
+    }
+    for (map<string,int>::iterator iM = mapcnt2.begin(); iM!=mapcnt2.end(); iM++)
+      printf("%03d\t%s\t\t%d\n",gameid,iM->first.c_str(),iM->second);
+}
+string tostring(vector<int> VI) {
+  stringstream ss;
+  for(int i = 0 ; i < VI.size(); i++) ss << VI[i]<<".";
+//  for (int i = 0 ; i < VI.size(); i++) cout << VI[i] <<",";
+  string s; ss >> s;   return s;
+}
+int dfs(int root,int mask,int mode) {
    if (lch[root]<0) return 0;
    int todel = data[lch[root]];
    //COUNT_situation
+   if (mode == 0) {
    for (int i = 0,im = 1 ; i < map_n; i++,im<<=1)
      if (graph[todel][i] && ((im & mask)==0)) {
         P2 p = make_pair(degree[i],degree[todel]);
         mapcnt[p]++;
      }
-
+   }
+   if (mode == 1) {
+     vector<int> VI; VI.clear();
+     for (int i = 0 , im = 1; i< map_n; i++, im<<=1) 
+       if (graph[todel][i] && ((im & mask) ==0))
+         VI.push_back(degree[i]);
+     sort(VI.begin(),VI.end());
+//     cout << tostring(VI)<< endl;
+     mapcnt2[tostring(VI)]++;
+   }
 
    for (int i = 0,im = 1 ; i < map_n; i++,im<<=1)
      if (graph[todel][i] && ((im & mask)==0)) degree[i] --;
 
-   dfs(lch[root], mask | (1<<todel));
+   dfs(lch[root], mask | (1<<todel), mode);
    for (int i = 0,im = 1 ; i < map_n; i++,im<<=1)
      if (graph[todel][i] && ((im & mask)==0)) degree[i] ++;
 
